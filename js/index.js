9e6dd3084a76e58831e8dcc8ddf978ef6df05658
Gerrit Lane <lane.gerrit@gmail.com>
2022-08-11 19:13:24 -0700

    Add mobile styling and reset die before display

--- js/index.js
+++ js/index.js
@@ -56,10 +56,19 @@ function displayNum (dieNum) {
 			]
 		}
 	console.log(dieArray)
+	
+	//Reset all dice dots
 	for (let i = 0; i < dieArray.length; i++) {
 		for (let j = 0; j < dieArray[i].length; j++) {
-			if (dieArray[i][j] === 0) {
-				document.querySelector(`.die :nth-child(${i+1}) :nth-child(${j+1})`).classList.add("blank")
+			document.querySelector(`.die :nth-child(${i+1}) :nth-child(${j+1})`).classList.add("blank")
+		}
+	}
+
+	// Set dice dots to match number rolled
+	for (let i = 0; i < dieArray.length; i++) {
+		for (let j = 0; j < dieArray[i].length; j++) {
+			if (dieArray[i][j] === 1) {
+				document.querySelector(`.die :nth-child(${i+1}) :nth-child(${j+1})`).classList.remove("blank")
 			}
 		}
 	}
